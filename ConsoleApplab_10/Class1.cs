34f0bc7edeba83baffd72a3cfcb5984274f8d6a2
Ira <mazuririnochkacat@gmail.com>
2023-04-26 16:36:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplab_10/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplab_10
+{
+    namespace Point3D_
+    {
+        public class Point3D
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public double Z { get; set; }
+
+            public Point3D(double x, double y, double z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+        }
+
+        public class PointsChangedEventArg : EventArgs
+        {
+            public Point3D[] Points { get; set; }
+        }
+        // делегат
+        public delegate void PointsChangedEventHandler(object sender, PointsChangedEventArg e);
+
+        public class Point3DList
+        {
+            private Point3D[] _points;
+
+            public event PointsChangedEventHandler PointsChanged;
+
+            public Point3DList(Point3D[] points)
+            {
+                _points = points;
+            }
+
+            public Point3D[] Points
+            {
+                get
+                {
+                    return _points;
+                }
+                set
+                {
+                    _points = value;
+                    OnPointsChanged(new PointsChangedEventArg { Points = value });
+                }
+            }
+
+            protected virtual void OnPointsChanged(PointsChangedEventArg e)
+            {
+                PointsChanged?.Invoke(this, e);
+            }
+        }
+    }
+}
---
+++ ConsoleApplab_10/Program.cs
@@ -0,0 +1,75 @@
+﻿using ConsoleApplab_10.Point3D_;
+
+namespace ConsoleApplab_10
+{
+    public class Program
+    {
+        public static double Distance(Point3D point1, Point3D point2)
+        {
+            double xDiff = point1.X - point2.X;
+            double yDiff = point1.Y - point2.Y;
+            double zDiff = point1.Z - point2.Z;
+           
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff + zDiff * zDiff);
+        }
+
+        public static void MaxDistance(Point3D[] points, out Point3D point1, out Point3D point2, out double maxDistance)
+        {
+            maxDistance = 0;
+            point1 = null;
+            point2 = null;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    double distance = Distance(points[i], points[j]);
+
+                    if (distance > maxDistance)
+                    {
+                        maxDistance = distance;
+                        point1 = points[i];
+                        point2 = points[j];
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Point3D[] points = new Point3D[3];
+            points[0] = new Point3D(1, 2, 3);
+            points[1] = new Point3D(4, 5, 6);
+            points[2] = new Point3D(7, 8, 9);
+
+            Point3DList pointList = new Point3DList(points);
+
+            pointList.PointsChanged += (sender, e) =>
+            {
+                Console.WriteLine("Points changed:");
+                foreach (var point in e.Points)
+                {
+                    Console.WriteLine($"({point.X}, {point.Y}, {point.Z})");
+                }
+            };
+
+            Console.WriteLine("Initial points:");
+            foreach (var point in pointList.Points)
+            {
+                Console.WriteLine($"({point.X}, {point.Y}, {point.Z})");
+            }
+            
+
+            Point3D point1;
+            Point3D point2;
+            double maxDistance;
+
+            Console.WriteLine($"Distance between point1 and point2 is {Distance(points[0], points[1])}");
+            MaxDistance(pointList.Points, out point1, out point2, out maxDistance);
+
+            Console.WriteLine($"Max distance between points ({point1.X}, {point1.Y}, {point1.Z}) and ({point2.X}, {point2.Y}, {point2.Z}) is {maxDistance}");
+            
+        }
+    }
+
+}
